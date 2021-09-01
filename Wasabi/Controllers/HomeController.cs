97970c6f88e27d4197b5b6338b0e4a471db9131e
Muhammad Sajid <muhammad.sajid@mparsec.com>
2021-09-01 19:14:24 +0500

    Add project files.

---
+++ Wasabi/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wasabi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private static IAmazonS3 _s3Client;
+        private const string _bucketName = "directory1";
+        private const string _objectName1 = "testFile.txt";
+
+        [HttpPost("Config")]
+        public async Task<bool> Config()
+        {
+            // 1. this is necessary for the endpoint
+            var config = new AmazonS3Config { ServiceURL = "https://s3.wasabisys.com" };
+            try
+            {
+                // create s3 connection with credential files and config.
+                _s3Client = new AmazonS3Client("PMW2RNR6S4LGVSL893N0", "AD7ACmebdK0Bmqj337vpZh9loX2S3553M6GKMRJF", config);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        [HttpPost("Upload_File_In_Bucket")]
+        public async Task<string> UploadFileAsync()
+        {
+            // Set Up creadentials
+            bool config = await Config();
+            if (!config)
+                return "File Not Upoaded!";
+
+            try
+            {
+                // Give the path of your file here that includes the file name.
+                var filePath = $"C:/Users/muhammad.sajid/OneDrive - MPARSEC LTD/Desktop/oop.txt";
+
+                var putRequest = new PutObjectRequest
+                {
+                    BucketName = _bucketName, // Name your directory already created in Wasabi Cloud Storage.
+                    Key = _objectName1, // Your file will be saved with this name.
+                    FilePath = filePath
+                };
+                putRequest.Metadata.Add("x-amz-meta-title", "SomeTitle");
+                await _s3Client.PutObjectAsync(putRequest);
+            }
+            catch (AmazonS3Exception e)
+            {
+                return e.Message;
+            }
+            return "OK";
+        }
+
+        [HttpPost("Get_File")]
+        public async Task<string> GetFileAsync()
+        {
+            // Set Up creadentials
+            bool config = await Config();
+            if (!config)
+                return "File Not Upoaded!";
+
+            // create a get object request
+            var getObjectRequest = new GetObjectRequest()
+            {
+                BucketName = _bucketName,
+                Key = _objectName1
+            };
+
+            // call a get operation
+            var res = await _s3Client.GetObjectAsync(getObjectRequest);
+            return "OK";
+        }
+    }
+}
---
+++ Wasabi/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wasabi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Version = "CHMSAJID7@GMAIL.COM", Title = "CHMSAJID7", Description = "chmsajid7@gmail.com" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                string swaggerJsonBasePath = string.IsNullOrWhiteSpace(x.RoutePrefix) ? "." : "..";
+                x.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "WASABI v1");
+            });
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
